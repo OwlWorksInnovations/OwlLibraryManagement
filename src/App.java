4f7f6b864c9775d70fe4d16bb783dea309e3b01a
OwlWorksInnovations <juanytfortnite@gmail.com>
2025-09-30 18:44:54 +0200

    Refactor App.java to improve code readability with better comments and formatting

--- src/App.java
+++ src/App.java
@@ -3,17 +3,21 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
+        // Lists
         List<BookObject> books = new ArrayList<BookObject>();
         List<String> booksNames = new ArrayList<String>();
         List<String> booksAuthors = new ArrayList<String>();
         List<String> booksSubjects = new ArrayList<String>();
 
+        // Adding books
         BookObject book = new BookObject("Mindset", "Carol S. Dweck", "Educational");
         BookObject book2 = new BookObject("Test", "te S. Dweck", "test");
-
+        
+        // Add books to list
         books.add(book);
         books.add(book2);
-
+        
+        // Loop through list and adds book info to lists
         for (BookObject iBook : books) {
             String bookName = iBook.getName();
             String bookAuthor = iBook.getAuthor();
@@ -23,10 +27,13 @@ public class App {
             booksAuthors.add(bookAuthor);
             booksSubjects.add(bookSubject);
         }
+
+        // Prints to console for testing
         System.out.println("Books names: " + booksNames + " authors " + booksAuthors + " subjects " + booksSubjects);
     }
 }
 
+// Book class
 class BookObject {
     private String name;
     private String author;
