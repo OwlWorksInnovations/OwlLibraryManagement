84a586d17253cca98385485a61b83820cc4ac26f
OwlWorksInnovations <juanytfortnite@gmail.com>
2025-09-30 18:40:39 +0200

    Implement book management features: add multiple books and display their details

--- src/App.java
+++ src/App.java
@@ -1,7 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
+        List<BookObject> books = new ArrayList<BookObject>();
+        List<String> booksNames = new ArrayList<String>();
+        List<String> booksAuthors = new ArrayList<String>();
+        List<String> booksSubjects = new ArrayList<String>();
+
         BookObject book = new BookObject("Mindset", "Carol S. Dweck", "Educational");
-        System.out.println("Book Created: " + book.getName() + " by " + book.getAuthor() + " for " + book.getSubject());
+        BookObject book2 = new BookObject("Test", "te S. Dweck", "test");
+
+        books.add(book);
+        books.add(book2);
+
+        for (BookObject iBook : books) {
+            String bookName = iBook.getName();
+            String bookAuthor = iBook.getAuthor();
+            String bookSubject = iBook.getSubject();
+
+            booksNames.add(bookName);
+            booksAuthors.add(bookAuthor);
+            booksSubjects.add(bookSubject);
+        }
+        System.out.println("Books names: " + booksNames + " authors " + booksAuthors + " subjects " + booksSubjects);
     }
 }
 
