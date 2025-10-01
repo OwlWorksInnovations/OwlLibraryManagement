19ab4a5d1cc95c4ca86377dd505a83a7e8431bbd
OwlWorksInnovations <juanytfortnite@gmail.com>
2025-10-01 12:36:36 +0200

    Added a option menu

--- src/App.java
+++ src/App.java
@@ -8,62 +8,110 @@ public class App {
     public static void main(String[] args) {
         // Lists
         List<BookObject> books = new ArrayList<BookObject>();
+        Boolean running = true;
 
-        // Adding books
-        BookObject book = new BookObject("Mindset", "Carol S. Dweck", "Educational");
-        BookObject book2 = new BookObject("Test", "Test", "test");
-        
-        // Add books to list 
-        books.add(book);
-        books.add(book2);
-        
         // Gets user input from console
-        Scanner sc = new Scanner(System.in); System.out.println("Enter a book name to search.");
-        String name = sc.next(); System.out.println("Enter a book name to delete."); 
-        String dName = sc.next();
-        
-        // Pattern searching for finding books and deleting them
-        Pattern searchPattern = Pattern.compile("\\b" + name + "\\b", Pattern.CASE_INSENSITIVE);
-        Pattern deletePattern = Pattern.compile("\\b" + dName + "\\b", Pattern.CASE_INSENSITIVE);
-        
-        // For if statements
-        boolean found = false;
-        boolean dFound = false;
-        
-        // Loop through list and adds book info to lists
-        java.util.Iterator<BookObject> iterator = books.iterator();
-        while (iterator.hasNext()) {
-            BookObject iBook = iterator.next();
-            String bookName = iBook.getName();
-            String bookAuthor = iBook.getAuthor();
-            String bookSubject = iBook.getSubject();
+        Scanner sc = new Scanner(System.in);
+        while (running) {
+            // Choice menu
+            System.out.println("Choose a menu option. [1] Search book [2] Delete book [3] List books [4] Add book [5] Exit");
+            Integer choice = Integer.parseInt(sc.nextLine());
 
-            Matcher searchMatcher = searchPattern.matcher(bookName);
-            while (searchMatcher.find()) {
-                System.out.println("Found book: " + searchMatcher.group() + " by " + bookAuthor + ", subject " + bookSubject);
-                found = true;
+            if (choice == 1) {
+                // Get info
+                System.out.println("Enter a book name to search.");
+                String name = sc.nextLine(); 
+
+                // Patern searching
+                Pattern searchPattern = Pattern.compile("\\b" + name + "\\b", Pattern.CASE_INSENSITIVE);
+
+                // For if statments
+                boolean found = false;
+
+                // Loop through list and finds book through pattern matching
+                java.util.Iterator<BookObject> iterator = books.iterator();
+                while (iterator.hasNext()) {
+                    BookObject iBook = iterator.next();
+                    String bookName = iBook.getName();
+                    String bookAuthor = iBook.getAuthor();
+                    String bookSubject = iBook.getSubject();
+
+                    Matcher searchMatcher = searchPattern.matcher(bookName);
+                    while (searchMatcher.find()) {
+                        System.out.println("Found book: " + searchMatcher.group() + " by " + bookAuthor + ", subject " + bookSubject);
+                        found = true;
+                    }
+                }
+                // Handles invalid or wrong input
+                if (!found) {
+                    System.out.println("No matching book found.");
+                }
+            } else if (choice == 2) {
+                // Get info
+                System.out.println("Enter a book name to delete."); 
+                String dName = sc.nextLine();
+                
+                // Pattern searching
+                Pattern deletePattern = Pattern.compile("\\b" + dName + "\\b", Pattern.CASE_INSENSITIVE);
+                
+                // For if statements
+                boolean dFound = false;
+                
+                // Loop through list and finds book through pattern matching
+                java.util.Iterator<BookObject> iterator = books.iterator();
+                while (iterator.hasNext()) {
+                    BookObject iBook = iterator.next();
+                    String bookName = iBook.getName();
+                    String bookAuthor = iBook.getAuthor();
+                    String bookSubject = iBook.getSubject();
+
+                    Matcher deleteMatcher = deletePattern.matcher(bookName);
+                    while (deleteMatcher.find()) {
+                        System.out.println("Deleting book: " + deleteMatcher.group() + " by " + bookAuthor + ", subject " + bookSubject);
+                        iterator.remove();
+                        dFound = true;
+                    }
+                }
+
+                // Handles invalid or wrong input
+                if (!dFound) {
+                    System.out.println("No matching book found for deletion.");
+                }
+            } else if (choice == 3) {
+                // Loop through list and finds book through pattern matching
+                java.util.Iterator<BookObject> iterator = books.iterator();
+                while (iterator.hasNext()) {
+                    BookObject iBook = iterator.next();
+                    String bookName = iBook.getName();
+                    String bookAuthor = iBook.getAuthor();
+                    String bookSubject = iBook.getSubject();
+
+                    System.out.println("Book: " + bookName + " by " + bookAuthor + ", subject " + bookSubject);
+                }
+            } else if (choice == 4) {
+                // Get book details
+                System.out.println("Enter book name: ");
+                String bookName = sc.nextLine();
+                System.out.println("Enter book author: ");
+                String bookAuthor = sc.nextLine();
+                System.out.println("Enter book subject: ");
+                String bookSubject = sc.nextLine();
+
+                // Adding books
+                BookObject book = new BookObject(bookName, bookAuthor, bookSubject);
+                
+                // Add books to list       
+                books.add(book);
+            } else if (choice == 5) {
+                System.out.println("Exiting...");
+                running = false;
+            } else {
+                System.out.println("Invalid choice or input.");
             }
-            
-            Matcher deleteMatcher = deletePattern.matcher(bookName);
-            while (deleteMatcher.find()) {
-                System.out.println("Deleting book: " + deleteMatcher.group() + " by " + bookAuthor + ", subject " + bookSubject);
-                iterator.remove();
-                dFound = true;
-            }
         }
         
-        if (!found) {
-            System.out.println("No matching book found.");
-        }
-
-        if (!dFound) {
-            System.out.println("No matching book found for deletion.");
-        }
-
         // Debug printing
         System.out.println(books);
-
-        // Prints to console for testing
         sc.close();
     }
 }
@@ -83,7 +131,7 @@ class BookObject {
     public String getName() {
         return name;
     }
-
+    
     public String getAuthor() {
         return author;
     }
